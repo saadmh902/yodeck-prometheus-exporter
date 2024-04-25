8cc7d7aef78107ccd8b5d1529aa56e0f47bccae5
saadmh902 <49423626+saadmh902@users.noreply.github.com>
2024-04-25 12:20:14 -0400

    Update yodeck-exporter.py

--- yodeck-exporter.py
+++ yodeck-exporter.py
@@ -3,6 +3,9 @@ import time
 import requests
 import json
 
+apiKey = ""
+username = ""
+
 class MyHandler(http.server.BaseHTTPRequestHandler):
     def _set_headers(self):
         self.send_response(200)
@@ -27,19 +30,23 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
             displayedText = "# TYPE yodeck_device_status gauge\n"
             exportString = ""
             print("Starting to collect data...")
-            r=requests.get("https://app.yodeck.com/api/v1/device/?username=[YOUR USERNAME]&api_key=[YOUR API KEY]&format=json")
+            r=requests.get("https://app.yodeck.com/api/v1/device/?username="+username+"&api_key="+apiKey+"&format=json")
     
         
             jsonObject = json.loads(r.text)
 
             for count,yodeckDevice in enumerate(jsonObject["objects"]):
-                exportString = ('yodeck_device_status{{ID="{}",name="{}",IsOnline="{}"}}\n').format(yodeckDevice['id'],yodeckDevice["name"],yodeckDevice["online"])
+                if(yodeckDevice['online'] == False):
+                    onlineStatus = "0"
+                else:
+                    onlineStatus = "1"
+                exportString = ('yodeck_device_status{ID="'+str(yodeckDevice['id'])+'",name="'+yodeckDevice['name']+'",IsOnline="'+str(yodeckDevice['online'])+'"} ' + onlineStatus+"\n")
                 #print(exportString)
                 displayedText+=exportString
 
 
 
-            displayedText += "# TYPE request_processing_seconds summary\n"
+            displayedText += "# TYPE request_processing_seconds summary\n" 
             displayedText = displayedText + 'request_processing_seconds ' + str(time.monotonic() - start_time) + '\n'
             print(displayedText)
             print(("Collected {} values").format(len(jsonObject["objects"])))
@@ -50,7 +57,7 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
 
 
 HTTP_BIND_IP = "localhost"
-HTTP_PORT_NUMBER = 3303
+HTTP_PORT_NUMBER = 3304
 
 server_class = MyHandler
 httpd = http.server.ThreadingHTTPServer((HTTP_BIND_IP, HTTP_PORT_NUMBER), server_class)
